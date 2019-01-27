adfa866b1443285a8c455e997e2436ca3f4c6b7b
Aayush Nirwan <35112082+aayushnirwan@users.noreply.github.com>
2019-01-27 20:09:00 +0530

    Update and rename downloads3.py to Download.py

--- Download_from_Amazon_S3_Bucket/Download.py
+++ Download_from_Amazon_S3_Bucket/Download.py
@@ -3,11 +3,14 @@ import sys, os
 from boto.s3.key import Key
 
 LOCAL_PATH = ' '
-KEY_ID = 'AKIAJ73OFBSSBSFIJRWQ'
-ACCESS_KEY = 'CgtsMkP6r85mXrwUVIEMR7wSZkgJtt0R9nIX0Vtr'
+
+#Put your id and key here
+KEY_ID = ''
+ACCESS_KEY = ''
 
 s3_connection = boto.connect_s3(KEY_ID,ACCESS_KEY)
 
+#Bucket name = tst008
 bucket = s3_connection.get_bucket('tst008')
 
 bucket_list = bucket.list()
