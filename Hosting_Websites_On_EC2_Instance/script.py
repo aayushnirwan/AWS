08d00842f594597be6c26135220b8dde7201b70b
Aayush Nirwan <35112082+aayushnirwan@users.noreply.github.com>
2019-01-27 20:16:47 +0530

    Update script.py

--- Hosting_Websites_On_EC2_Instance/script.py
+++ Hosting_Websites_On_EC2_Instance/script.py
@@ -2,5 +2,5 @@ import boto.ec2
 from boto.manage.cmdshell import sshclient_from_instance
 conn=boto.ec2.connect_to_region('us-east-2')
 instance = conn.get_all_instances(['i-0ae3a6792468c19c5'])[0].instances[0]
-ssh_client = sshclient_from_instance(instance,'newkeypair.pem',user_name='ec2-user')
+ssh_client = sshclient_from_instance(instance,'newkeypair.pem',user_name='ec2-user') # Put your key pair filename in place of newkeypair.pem
 print ssh_client.run('sudo yum install -y httpd')
