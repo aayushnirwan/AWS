4f2d1815d1fa662f0c9a986eb1132713a95408ba
Aayush Nirwan <35112082+aayushnirwan@users.noreply.github.com>
2019-01-27 20:06:56 +0530

    Update and rename uploads3.py to Upload.py

--- Upload_to_Amazon_S3_Bucket/Upload.py
+++ Upload_to_Amazon_S3_Bucket/Upload.py
@@ -2,11 +2,13 @@ import boto
 import boto.s3
 from boto.s3.key import Key
 
-KEY_ID = 'AKIAJ73OFBSSBSFIJRWQ'
-ACCESS_KEY = 'CgtsMkP6r85mXrwUVIEMR7wSZkgJtt0R9nIX0Vtr'
+#Put your id and key here
+KEY_ID = ''
+ACCESS_KEY = ''
 
 s3_connection = boto.connect_s3(KEY_ID,ACCESS_KEY)
 
+#bucket name = tst008
 bucket = s3_connection.create_bucket('tst008',location=boto.s3.connection.Location.DEFAULT)
 
 k = Key(bucket)
